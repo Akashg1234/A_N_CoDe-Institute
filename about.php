cf9a0b431b8151bc6a8ea77b51089923977f1c1b
Akash Goswami <46678116+Akashg1234@users.noreply.github.com>
2020-05-06 20:09:37 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,83 @@
+<?php
+session_start();
+include "connect.php";
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+   <title>About this project</title>
+<script src="https://kit.fontawesome.com/4ce794378e.js" crossorigin="anonymous"></script>   
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script
+  src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+  integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="
+  crossorigin="anonymous"></script>
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> 
+<style type="text/css">
+   a{text-decoration: none;}
+   a:hover{text-decoration: none;}
+</style>  
+</head>
+<body style=
+   "margin: 0px;
+   padding: 0px;
+   background-image: linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6));">
+<nav class="navbar bg-dark navbar-expand-lg navbar-dark" id="navbarNav">
+      <ul class="navbar-nav" style="list-style: none;">
+         <li class="nav-item" style="font-size:18px;">
+            <a href="index.php" class=" nav-link text-white " style="margin-left:  0px; font-size:18px;">Home </a>
+         </li>
+         <li class="nav-item">
+            <a href="cources.php" class=" nav-link text-white" style="margin-left:  15px; font-size:18px;"> Cources </a>
+         </li>
+         <li class="nav-item">
+            <a href="about.php" class=" nav-link text-white font-weight-bold" style="margin-left:  15px; font-size:18px;"> About </a>
+         </li>
+         <li class="nav-item">
+            <a href="notice.php" class=" nav-link text-white" style="margin-left:  15px; font-size:18px;"> Notice </a>
+         </li>
+         <li class="nav-item">
+            <a href="contact.php" class=" nav-link text-white" style="margin-left:  15px;font-size:18px;"> Contact </a>
+         </li>
+         <li class="nav-item">
+            <a href="exameforum.php" class=" nav-link text-white" style="margin-left:  15px;font-size:18px;width: 125px; "> Exame Forum </a>
+         </li>
+         <li class="nav-item"><a href="faq.php" class=" nav-link text-white" style="margin-left: 15px;font-size:18px;"> FAQ </a>
+         </li>
+         <li class="nav-item rounded" style="float: right; margin-left: 800px; background-image: linear-gradient(90deg,#ff3838,#ff4d4d,#F97F51,#f9ca24); width: 70px;"><a href="login.php" class="navbar-brand nav-link text-white"> Login </a>
+         </li>
+      </ul>
+</nav>   
+<div class="w-50 mt-4 pt-5 pl-5 pr-5 pb-0 ml-2 shadow rounded bg-white">
+   <p>
+      When I was start this project, I have no idea about the web technologies.
+      I start is with the basic HTML and CSS . After some time I made the home page by it's help and continuously try to make it's front end beautiful. And simoultaneously leaning PHP for it's back end development. With the learning of PHP I try to integrate it with the web pages. After a time I learn the MySQL database connection with PHP and I implemented it into this project. After this I try to learn fetching data from the database, and it was work finely. Then I gaining the knowledge about database operation like searching ,insert , update, delete values from the database.
+      And try it to implement through PHP, and it was succesful. Suddenly one day I have seen my virtual server is not working properly, so I decided to reinstall my server. Before that I backup my data from the database and when I try to backup my pages from the working directories , I noticed some files are missing . I was one of the horrible situation in my life, because my 7 to 8 mounth hard worked project more than fifty percent was spoiled. I was really depressed . I have decided that I will make this project again. Now my project is completed within the half of the previous work time, because of my previous experience help me a lot to develop this project. I made a educational institute's website with help of PHP. And there I have tried to add all necessary options for the all respected users.
+             <br > <div class="font-weight-bold " style="margin-left: 400px;"> -Akash Goswami</div></br>
+   </p>
+</div>
+<div class="w-50  p-5 mr-2 rounded" style="margin-left: 50%; margin-top: -560px;">
+      <img src="pp.jpg" class="rounded-circle shadow" style="width: 200px;height: 200px;margin-left: 35%;border: 2px solid white;"><br><br>
+      <div class="w-100 btn-toolbar d-flex justify-content-center
+      " role="toolbar">
+         <div class="btn-group " role="group">
+            <a href="https://www.linkedin.com/in/mr-akash-goswami-24b33b1a1"  class="btn btn-primary text-white p-2" style="width: 55px;height: 53px;text-align:center;">
+                  <i class="fab fa-linkedin fa-2x" class="mt-1" style=""></i>
+            </a>
+               
+            <a href="https://twitter.com/AkashGo61102371" class="btn btn-light text-primary p-2" style="width: 55px;height: 53px;text-align:center;">
+                  <i class="fab fa-twitter fa-2x" class="mt-1" style=""></i>
+            </a>
+               
+            <a href="https://github.com/Akashg1234" class="btn btn-dark text-white p-2" style="width: 55px;height: 53px;text-align:center;">
+                  <i class="fab fa-github fa-2x" class="mt-1" ></i>
+            </a>
+         </div>
+      </div>
+</div>
+</body>
+</html>
